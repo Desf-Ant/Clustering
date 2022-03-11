ae0d21600752c7d1cc2389891d99716e989fbb37
Antoine D <a.desfaits@orange.fr>
2022-03-11 09:36:22 +0100

    readd dbscanNote

--- src/clustering/dbscannote.h
+++ src/clustering/dbscannote.h
@@ -1,11 +1,31 @@
 #ifndef DBSCANNOTE_H
 #define DBSCANNOTE_H
 
+#include "../core/distance.h"
+#include "../core/note.h"
+#include "../core/notes.h"
+#include <vector>
+#include <iterator>
 
-class dbscanNote
+class DbscanNote
 {
-public:
-    dbscanNote();
-};
+private :
+    Notes* donnees;
+    std::vector <Note>* lesDonnees;
+    type_dist type_distance;
+    Distance distance;
+    unsigned int nb_points_minimum;
+    double distance_maximum;
 
+    unsigned int classe_courante;
+    std::vector<bool> deja_visites;
+    std::vector<unsigned int> voisinage_courant;
+    std::vector<unsigned int> a_ajouter_au_cluster;
+
+public:
+    DbscanNote ( Notes* d, type_dist t );
+    void execute ( void );
+    void calculVoisinage ( unsigned int indice );
+    void etendCluster ( unsigned int indice );
+};
 #endif // DBSCANNOTE_H
